cab235a0b1b1dc94c9d8bec8ba8025b01ef0ce89
BenardAbwao <benard.okoth.abwao@gmail.com>
2022-06-10 00:38:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Code your solution here
 
 function findMatching (list, name) {
-    return list.filter(function (driverName) {
+    return list.filter( driverName => {
       return driverName.toLowerCase() === name.toLowerCase();
     });
   }
   function fuzzyMatch (list, partialName) {
     let lengthOfName = partialName.length;
-    return list.filter(function (driverName) {
+    return list.filter( driverName => {
       return driverName.slice(0, lengthOfName) === partialName;
     });
   }
   function matchName (list, name) {
-    return list.filter(function (driver) {
+    return list.filter(driver => {
       return driver.name.toLowerCase() === name.toLowerCase();
     });
   } 
